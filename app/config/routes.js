cc77a637ab0ed3b2e5ed27c478c895119857e3c4
Udit Manektala <code@getbraintree.com>
2016-03-04 15:18:34 -0800

    Adding routing

---
+++ app/config/routes.js
@@ -0,0 +1,19 @@
+var React = require('react');
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var IndexRoute = ReactRouter.IndexRoute;
+var hashHistory = ReactRouter.hashHistory;
+var Main = require('../components/Main');
+var Home = require('../components/Home');
+
+
+var Routes = (
+  <Router>
+    <Route path='/' component={Main}>
+      <IndexRoute path='/home' component={Home} />
+    </Route>
+  </Router>
+
+)
+module.exports = Routes;
--- app/index.js
+++ app/index.js
@@ -1,54 +1,8 @@
-var USER_DATA = {
-  name: 'Udit',
-  username: 'udit99',
-  imageUrl: 'https://avatars3.githubusercontent.com/u/154256?v=3&s=400'
-}
-
-
 var React = require('react');
 var ReactDOM = require('react-dom');
+var routes = require('./config/routes')
 
-var ProfilePic = React.createClass({
-  render: function(){
-    return (
-      <image src={this.props.imageUrl} style={{height: 100, width: 100}}/>
-    )
-  }
-})
-
-var ProfileLink = React.createClass({
-  render: function(){
-    return (
-      <div>
-        <a href={"http://www.github.com/" + this.props.username} >
-          {this.props.username}
-        </a>
-      </div>
-    )
-  }
-})
-
-var ProfileName = React.createClass({
-  render: function(){
-    return (
-      <div>{this.props.name}</div>
-    )
-  }
-})
-
-
-var Avatar = React.createClass({
-  render: function(){
-    return (
-      <div>
-        <ProfileName name={this.props.user.name}/>
-        <ProfileLink username={this.props.user.username}/>
-        <ProfilePic imageUrl={this.props.user.imageUrl}/>
-      </div>
-    )
-  }
-})
 ReactDOM.render(
-  <Avatar user={USER_DATA}/>,
+  routes,
   document.getElementById('app')
 );
