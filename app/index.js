6310ee887f677f97b883bf32f182585bd57ee614
Udit Manektala <code@getbraintree.com>
2016-03-04 14:12:47 -0800

    Rendered Github component successfully

--- app/index.js
+++ app/index.js
@@ -1,14 +1,54 @@
+var USER_DATA = {
+  name: 'Udit',
+  username: 'udit99',
+  imageUrl: 'https://avatars3.githubusercontent.com/u/154256?v=3&s=400'
+}
+
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var Hello = React.createClass({
-  render: function () {
+var ProfilePic = React.createClass({
+  render: function(){
     return (
-      <div>Hello ReactJS Program!</div>
+      <image src={this.props.imageUrl} style={{height: 100, width: 100}}/>
     )
   }
-});
+})
+
+var ProfileLink = React.createClass({
+  render: function(){
+    return (
+      <div>
+        <a href={"http://www.github.com/" + this.props.username} >
+          {this.props.username}
+        </a>
+      </div>
+    )
+  }
+})
+
+var ProfileName = React.createClass({
+  render: function(){
+    return (
+      <div>{this.props.name}</div>
+    )
+  }
+})
+
+
+var Avatar = React.createClass({
+  render: function(){
+    return (
+      <div>
+        <ProfileName name={this.props.user.name}/>
+        <ProfileLink username={this.props.user.username}/>
+        <ProfilePic imageUrl={this.props.user.imageUrl}/>
+      </div>
+    )
+  }
+})
 ReactDOM.render(
-  <Hello/>,
+  <Avatar user={USER_DATA}/>,
   document.getElementById('app')
 );
