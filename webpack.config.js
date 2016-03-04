131e10c9251677d2d37615c80869139c7f647589
Udit Manektala <code@getbraintree.com>
2016-03-04 12:51:35 -0800

    doing the right fix by renaming babel.rc to .babelrc

--- webpack.config.js
+++ webpack.config.js
@@ -20,10 +20,7 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: "babel",
-        query: {
-          presets:['react']
-        }
+        loader: "babel-loader"
       }
     ]
   },
