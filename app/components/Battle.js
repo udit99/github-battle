fe5c6d5909016a7c81bb0c117cfb6fc2fb58ee2d
Udit Manektala <code@getbraintree.com>
2016-05-27 14:14:49 -0700

    styles

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -4,6 +4,7 @@ var ReactRouter = require('react-router');
 var Link = ReactRouter.Link;
 var UserDetails = require('./UserDetails');
 var UserDetailsWrapper = require('./UserDetailsWrapper');
+var styles = require('../styles/styles.js');
 
 function ConfirmBattle (props) {
   return props.isLoading === true
@@ -21,10 +22,10 @@ function ConfirmBattle (props) {
 
         </div>
         <div className='col-sm-8 col-sm-offset-2'>
-          <div className='col-sm-12'>
+          <div className='col-sm-12' style={styles.space}>
             <button type='button' className='btn btn-lg btn-success' onClick={props.onInitiateBattle}>Initiate Battle!</button>
           </div>
-          <div className='col-sm-12'>
+          <div className='col-sm-12' style={styles.space}>
             <Link to='/playerOne'>
               <button type='button' className='btn btn-lg btn-danger'>Reselect Players</button>
             </Link>
